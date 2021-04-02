3925796480e3c8044456883cb01f618fdb678ef6
Martin <martindaa@hotmail.no>
2021-04-02 13:06:07 +0200

    La til noen requests for å teste User og Users

--- client.py
+++ client.py
@@ -4,16 +4,15 @@ response = requests.post('http://127.0.0.1:5000/' + "/api/users", {"name": "Mart
 print(response.json())
 response = requests.post('http://127.0.0.1:5000/' + "/api/users", {"name": "Mart"})
 print(response.json())
-response = requests.post('http://127.0.0.1:5000/' + "/api/users", {"name": "Marti"})
-print(response.json())
 
 response = requests.delete('http://127.0.0.1:5000/' + "/api/user/0")
 print(response)
 
-response = requests.post('http://127.0.0.1:5000/' + "/api/users", {"name": "Martin"})
+response = requests.post('http://127.0.0.1:5000/' + "/api/users", {"name": "Marty"})
 print(response.json())
 
 response = requests.get('http://127.0.0.1:5000/' + "/api/users")
 print(response.json())
 
-
+response = requests.get('http://127.0.0.1:5000/' + "api/user/2")
+print(response.json())
