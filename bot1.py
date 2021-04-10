02225040f52319078f87bb59c6e39bb1bf870ce3
Martin <martindaa@hotmail.no>
2021-04-10 14:49:04 +0200

    Bot1 stuff

--- bot1.py
+++ bot1.py
@@ -12,15 +12,19 @@ def post_room(room):
     print(response.json())
 
 
-#def post_message(chat, user_id, room_id):
+def post_message(chat, user_id, room_id):
+    response = requests.post('http://127.0.0.1:5000/' + "/api/rooms/{}/{}/messager".format(room_id, user_id), {"chat": chat,"user_id":user_id, "room_id":room_id})
 
-
-#def get_all_messages(room_id, user_id):
+def get_all_messages(room_id, user_id):
+    response = requests.get('http://127.0.0.1:5000/' + "/api/rooms/" + room_id + "/" + user_id + "/messages")
+    print(response.json())
 
 
 #def get_all_messages_room(room_id, user_id):
 
 
 if __name__ == '__main__':
-    post_user(input("Write your name"))
-    post_room(input("Which room do you want to create? (Number)"))
+    post_user("Ulf")
+    post_room(4)
+    post_message("Hei, alle sammen", user_id, 4)
+    get_all_messages(4,user_id)
